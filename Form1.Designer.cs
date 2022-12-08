762469aad4598f250bf7dbb23f421900b28d0da1
jad723 <jadmaalouf2003.jm15@gmail.com>
2022-12-08 09:52:44 +0200

    Made GUI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,98 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtAmmo = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAmmo
+            // 
+            this.txtAmmo.AutoSize = true;
+            this.txtAmmo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmmo.ForeColor = System.Drawing.Color.White;
+            this.txtAmmo.Location = new System.Drawing.Point(13, 13);
+            this.txtAmmo.Name = "txtAmmo";
+            this.txtAmmo.Size = new System.Drawing.Size(93, 24);
+            this.txtAmmo.TabIndex = 0;
+            this.txtAmmo.Text = "Ammo: 0";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(372, 13);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(71, 24);
+            this.txtScore.TabIndex = 0;
+            this.txtScore.Text = "Kills: 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(650, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Health:";
+            // 
+            // healthBar
+            // 
+            this.healthBar.Location = new System.Drawing.Point(725, 14);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(187, 23);
+            this.healthBar.TabIndex = 1;
+            this.healthBar.Value = 100;
+            // 
+            // player
+            // 
+            this.player.Image = global::Zombie_Shooter_Game.Properties.Resources.up;
+            this.player.Location = new System.Drawing.Point(376, 540);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(71, 100);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 2;
+            this.player.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(924, 661);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtAmmo);
+            this.Name = "Form1";
+            this.Text = "Zombie Shooter Game";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label txtAmmo;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar healthBar;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer GameTimer;
     }
 }
 
