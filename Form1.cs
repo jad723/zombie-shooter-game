44399ec82ea940f62d0b54ac002082d639c001a4
jad723 <jadmaalouf2003.jm15@gmail.com>
2022-12-08 10:33:13 +0200

    Added controls

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,7 @@ namespace Zombie_Shooter_Game
         int playerHealth = 100;
         int speed = 10;
         int ammo = 10;
+        int score;
         int zombieSpeed = 3;
 
         Random randNum = new Random();
@@ -31,16 +32,68 @@ namespace Zombie_Shooter_Game
 
         private void MainTimerEvent(object sender, EventArgs e)
         {
+            if (playerHealth > 1) healthBar.Value = playerHealth;
+
+            else gameOver = true;
+
+            txtAmmo.Text = "Ammo: " + ammo;
+            txtScore.Text = "Kills: " + score;
+
+            if (goLeft == true && player.Left > 0) player.Left -= speed;
+
+            if (goRight == true && player.Left + player.Width < this.ClientSize.Width) player.Left += speed;
+
+            if(goUp == true && player.Top > 0) player.Top -= speed;
+
+            if(goDown == true && player.Top + player.Height < this.ClientSize.Height) player.Top += speed;
+            
 
         }
 
         private void KeyIsDown(object sender, KeyEventArgs e)
         {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+                facing = "left";
+                player.Image = Properties.Resources.left;
+            }
+
+            if(e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+                facing = "right";
+                player.Image = Properties.Resources.right;
+            }
+
+            if(e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+                facing = "up";
+                player.Image = Properties.Resources.up;
+            }
+
+            if(e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+                facing = "down";
+                player.Image = Properties.Resources.down;
+            }
 
         }
 
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.Left) goLeft = false;
+
+
+            if (e.KeyCode == Keys.Right) goRight = false;
+
+            if (e.KeyCode == Keys.Up) goUp = false;
+
+            if (e.KeyCode == Keys.Down) goDown = false;
+
+            if (e.KeyCode == Keys.Space) ShootBullet(facing);
 
         }
 
